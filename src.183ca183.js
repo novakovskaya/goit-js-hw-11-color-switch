be8630975908d4fd3f7ef96299f8e0d1358ed6fe
novakovskaya <81386471+novakovskaya@users.noreply.github.com>
2021-10-07 09:48:08 +0000

    Deploying to gh-pages from @ novakovskaya/goit-js-hw-11-color-switch@9507b5bf957a9cc98d03f3a65bc884829f98e79c 🚀

---
+++ src.183ca183.js
@@ -0,0 +1,12 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"clu1":[function(require,module,exports) {
+
+},{}],"oLA8":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.colors=void 0;const e=["#FFFFFF","#2196F3","#4CAF50","#FF9800","#009688","#795548"];exports.colors=e;
+},{}],"VyiV":[function(require,module,exports) {
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.default=void 0;var t={startBtn:document.querySelector('[data-action="start"]'),stopBtn:document.querySelector('[data-action="stop"]')};exports.default=t;
+},{}],"Kkkx":[function(require,module,exports) {
+"use strict";var t=require("./colors"),e=n(require("./refs"));function n(t){return t&&t.__esModule?t:{default:t}}const r=1e3;let l=null;function o(){l=setInterval(()=>{document.body.style.backgroundColor=t.colors[a(0,t.colors.length-1)]},r),e.default.startBtn.disabled=!0}function a(t,e){return Math.floor(Math.random()*(e-t+1)+t)}function d(){clearInterval(l),e.default.startBtn.disabled=!1}e.default.startBtn.addEventListener("click",o),e.default.stopBtn.addEventListener("click",d);
+},{"./colors":"oLA8","./refs":"VyiV"}],"Focm":[function(require,module,exports) {
+"use strict";require("./sass/main.scss"),require("./js/colorSwitch");
+},{"./sass/main.scss":"clu1","./js/colorSwitch":"Kkkx"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-11-color-switch/src.183ca183.js.map
